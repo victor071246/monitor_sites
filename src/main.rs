c5fd003df4f2dc4ae4b762409fce7cfc18278eed
v <daderiovictorfelipe@gmail.com>
2026-06-07 23:55:05 -0300

    refatorando projeto novamente, dessa vez estabelecendo dependencias pensando em sqlite com persistencia dos estados do monitor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,1 @@
-mod config;
-mod checker;
-
-fn main() {
-    let config = config::carregar();
-    for host in &config.monitor.hosts {
-        checker::check_host(host, config.monitor.port);
-    }
-}
+fn main() {}
